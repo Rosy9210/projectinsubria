dd3c2e2acb00156e120fb185320d419b9b80bf7d
Rosy <rosi90210@gmail.com>
2020-08-11 18:38:46 +0200

    barcode detector

--- app/src/main/java/it/uninsubria/pdm/trovaprezzi/MainActivity.java
+++ app/src/main/java/it/uninsubria/pdm/trovaprezzi/MainActivity.java
@@ -1,33 +1,46 @@
 package it.uninsubria.pdm.trovaprezzi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
-    TextView risultato;
-
+    TextView tvrisultato;
+    private Intent data;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
-        risultato= (TextView)findViewById ( R.id.tvrisultato );
+        tvrisultato= (TextView)findViewById ( R.id.tvrisultato );
     }
 
-    public void scatta(View view) {
 
-        Log.d ( "scattafoto", "Trova Prezzo");
-        Intent cameraIntent = new Intent ( MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(cameraIntent);
+    public void scanBarcode(View view){
+        Intent intent=new Intent(this,ScanBarcodeActivity.class);
+        startActivityForResult ( intent, 0 );
+    }
+    @Override
+    protected void onActivityResult(int RequestCode,int ResultCode,Intent data) {
+        if (RequestCode == 0) {
+            if (ResultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra ( "barcode" );
+                    tvrisultato.setText ( "Barcode value :" + barcode.displayValue );
+                } else {
+                    super.onActivityResult ( RequestCode, ResultCode, data );
+                }
+            } else {
+                tvrisultato.setText ( "no barcode found" );
+            }
+        }
     }
 
 }
